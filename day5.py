17761d74ce248e7d403334b1d1d4373723a85fa5
Michael Deng <michaelmdeng@gmail.com>
2018-12-04 22:28:23 -0800

    day5

---
+++ day5.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python
+from __future__ import print_function
+from shared import read_input
+
+INPUT_FILE_PATH = 'day5-input.txt'
+
+
+def process_reactions(chain):
+    polys = []
+
+    idx = 0
+    while idx < (len(chain) - 1):
+        poly_1 = chain[idx]
+        poly_2 = chain[idx + 1]
+
+        if poly_1.lower() == poly_2.lower() and poly_1 != poly_2:
+            idx += 2
+        else:
+            polys.append(poly_1)
+            idx += 1
+
+    if idx == (len(chain) - 1):
+        polys.append(chain[idx])
+
+    return ''.join([poly for poly in polys])
+
+
+def process_all(chain):
+    has_reactions = True
+    idx = 0
+    while has_reactions:
+        prev_length = len(chain)
+        chain = process_reactions(chain)
+        new_length = len(chain)
+
+        has_reactions = new_length != prev_length
+        idx += 1
+
+    return chain
+
+
+def remove_poly(chain, poly):
+    return chain.replace(poly.lower(), "").replace(poly.upper(), "")
+
+
+def main():
+    chain = read_input(INPUT_FILE_PATH)[0]
+    chain = chain[:len(chain) - 1]  # remove '\n'
+
+    new_chain = process_all(chain)
+    print('Part 1: ' + str(len(new_chain)))
+
+    lens = [len(process_all(remove_poly(chain, poly)))
+            for poly in 'abcdefghijklmnopqrstuvwyxz']
+    print('Part 2: ' + str(max(lens)))
+
+
+if __name__ == '__main__':
+    main()
