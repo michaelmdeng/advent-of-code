bff5bc8f64aa86a14467f14cdf52d8a7f5d42b70
Michael Deng <michaelmdeng@gmail.com>
2019-12-20 12:10:46 -0800

    Set up simple stack project and indent files with hindent

---
+++ haskell/src/Shared.hs
@@ -0,0 +1,24 @@
+module Shared where
+
+import System.IO
+
+app :: [String] -> String -> [String]
+app acc elem = acc ++ [elem]
+
+readAllHelper :: Handle -> [String] -> IO [String]
+readAllHelper handle acc = do
+  isEOF <- hIsEOF handle
+  out <-
+    if isEOF
+      then pure acc
+      else do
+        line <- hGetLine handle
+        lines <- readAllHelper handle (app acc line)
+        return lines
+  return out
+
+readAll :: FilePath -> IO [String]
+readAll path = do
+  handle <- openFile path ReadMode
+  lines <- readAllHelper handle []
+  return lines
