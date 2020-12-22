686a93db16b74b636d7edafcab9f5b09559f5921
Michael Deng <michaelmdeng@gmail.com>
2020-12-21 23:03:00 -0800

    2020 Day 9

---
+++ jvm/src/main/scala/advent/y2020/Day9.scala
@@ -0,0 +1,50 @@
+package advent.y2020
+
+import advent.shared.SafeDayRunner
+
+object Day9 extends SafeDayRunner[String, Long, Long] {
+  protected def YEAR: Int = 2020
+  protected def DAY: Int = 9
+
+  private val BUFFER_LENGTH: Int = 25
+
+  def validNext(buffer: Seq[Long], next: Long): Boolean = {
+    val members = Set(buffer: _*)
+    val diffs = Set(buffer.map(next - _): _*)
+    (diffs & members).nonEmpty
+  }
+
+  def safeRunPart1(lines: Seq[String]): Long = {
+    lines
+      .map(_.toLong)
+      .sliding(BUFFER_LENGTH + 1)
+      .find(bufferPlus => !validNext(bufferPlus.init, bufferPlus.last))
+      .map(_.last)
+      .get
+  }
+
+  def safeRunPart2(lines: Seq[String]): Long = {
+    val invalidNumber = safeRunPart1(lines)
+
+    Stream(lines.map(_.toLong): _*).tails
+      .map(tail => {
+        // Stream partial sums until we've surpassed the SECRET_NUMBER
+        // This we way don't have to calculate unecessary sums
+        val sums = tail
+          .scanLeft(0L)(_ + _)
+          .takeWhile(_ <= invalidNumber)
+          .tail
+          .force
+          .toSeq
+        (tail.take(sums.length), sums)
+      })
+      .find {
+        case (_, sums) => sums.last == invalidNumber
+      }
+      .map {
+        case (tail, _) => tail
+      }
+      .map(sequence => sequence.min + sequence.max)
+      .get
+  }
+}
