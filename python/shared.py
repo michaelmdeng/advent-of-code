a069384807cc74449ca8fcc79273bce975831822
Michael Deng <michaelmdeng@gmail.com>
2023-06-18 10:13:29 -0700

    feat: enable reading separate pt2 test input

--- python/shared.py
+++ python/shared.py
@@ -1,5 +1,14 @@
 import os
+import os.path
+from pathlib import Path
 from unittest import TestCase
+
+
+def file_exists(file_name):
+    root_path = os.path.abspath(os.path.dirname(__file__))
+    path = os.path.join(root_path, '../input/{}'.format(file_name))
+
+    return os.path.exists(path) and Path(path).is_file()
 
 
 def read_input(file_name):
@@ -24,15 +33,31 @@ class AdventDay:
 
     @property
     def input_data(self):
-        return read_input(self.input_file)
+        if file_exists(self.input_file):
+            return read_input(self.input_file)
 
     @property
     def example_input_file(self):
         return f'{self.year}/day{self.day}-test.txt'
 
     @property
+    def example_input_file_pt2(self):
+        return f'{self.year}/day{self.day}-pt2-test.txt'
+
+    @property
     def example_input_data(self):
         return read_input(self.example_input_file)
+
+    @property
+    def example_input_data_pt1(self):
+        return self.example_input_data
+
+    @property
+    def example_input_data_pt2(self):
+        if file_exists(self.example_input_file_pt2):
+            return read_input(self.example_input_file_pt2)
+        if file_exists(self.example_input_file):
+            return self.example_input_data
 
 
 class AdventDayRunner:
@@ -46,7 +71,7 @@ class AdventDayRunner:
         if not callable(runner):
             return
 
-        result = runner(self.instance.example_input_data)
+        result = runner(self.instance.example_input_data_pt1)
 
         print(f'{self.__class__.instance_cls.__name__} Part 1 Example Result: {result}')
 
@@ -68,7 +93,7 @@ class AdventDayRunner:
         if not callable(runner):
             return
 
-        result = runner(self.instance.example_input_data)
+        result = runner(self.instance.example_input_data_pt2)
 
         print(f'{self.__class__.instance_cls.__name__} Part 2 Example Result: {result}')
 
